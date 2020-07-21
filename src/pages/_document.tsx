11ec1bfe528d890034f6046a2c8a2eb2c74913b6
satokiyoshino <syoshino@toriyama.me>
2020-07-21 12:06:44 +0900

    add apple touch icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,8 @@ class CustomDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width,height=device-height" key="viewport" />
           <link rel='manifest' href='../static/manifest.json' />
-          <link rel='icon' type='image/png' href='./icons/icon-512x512.png' />
+          <link rel='icon' type="image/png" href='./icons/icon-512x512.png' />
+          <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="./icons/icon-192x192.png" />
         </Head>
 
         <body>
