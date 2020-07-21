af38caacc07dd2d8155029bbfc94963930857d20
satokiyoshino <syoshino@toriyama.me>
2020-07-21 11:41:59 +0900

    add document and PWA

---
+++ next.config.js
@@ -0,0 +1,5 @@
+// withOffline を読み込む
+const withOffline = require('next-offline')
+
+// nextConfig を withOffline に渡す
+module.exports = withOffline({})
---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+
+class CustomDocument extends Document {
+  render() {
+    return (
+      <Html lang='ja'>
+        <Head>
+          <meta name="viewport" content="width=device-width,height=device-height" key="viewport" />
+          <link rel='manifest' href='../static/manifest.json' />
+        </Head>
+
+        <body>
+          <Main />
+
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
